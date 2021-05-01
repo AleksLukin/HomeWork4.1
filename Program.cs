aa2484ec2ece12d5c23ec41f9b37d7f978f82564
AleksLukin <75177874+AleksLukin@users.noreply.github.com>
2021-05-01 09:47:02 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+        public void RandomStrings(string[] words)
+        {
+            Random rnd = new Random();
+            string word = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    word += (char)rnd.Next(97, 123);
+                }
+                words[i] = word;
+                word = "";
+            }
+        }
 
 
