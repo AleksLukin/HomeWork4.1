c233df8a9d2a5d5b3830f685e3758676f4186921
AleksLukin <75177874+AleksLukin@users.noreply.github.com>
2021-05-01 23:31:39 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,1 @@
-        public void RandomStrings(string[] words)
-        {
-            Random rnd = new Random();
-            string word = "";
-            for (int i = 0; i < words.Length; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    word += (char)rnd.Next(97, 123);
-                }
-                words[i] = word;
-                word = "";
-            }
-        }
+
